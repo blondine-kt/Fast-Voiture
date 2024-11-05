2d6921354143df779d46cfc19d42bffcaa9b8ac9
blondine-kt <blondinekeumani@teccart.online>
2024-11-04 20:39:06 -0500

    New Structure

--- FastVoiture_WEB/Fast-Voiture/src/components/Header.jsx
+++ FastVoiture_WEB/Fast-Voiture/src/components/Header.jsx
@@ -7,6 +7,13 @@ function Header() {
       className={`${styles.header_container} d-flex flex-row align-items-center`}
     >
       <img src="../../assets/images/fv_icon.png" alt="FastVoiture icon" />
+
+      <div>
+        <ul>
+          <li>Connexion</li>
+          <li>Inscription</li>
+        </ul>
+      </div>
     </div>
   );
 }
